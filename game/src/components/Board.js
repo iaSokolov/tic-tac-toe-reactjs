76326fc8e5fe9f266ef6e279a126e628294dbd22
Ilya Sokolov <iasokolov@gmail.com>
2022-02-20 15:59:51 +0300

    move all component data into a Game Component

--- game/src/components/Board.js
+++ game/src/components/Board.js
@@ -2,65 +2,13 @@ import React from 'react';
 import Square from './Square'; 
 
 class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squareItem: Array(9).fill(null),
-            isPlayerCurrent: true,
-            winner: null
-        };
-    }
-
-    calcWinner(squareItem) {
-        let ret = null;
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squareItem[a] &&
-                squareItem[a] === squareItem[b] &&
-                squareItem[a] === squareItem[c]) {
-                ret = squareItem[a];
-                break;
-            }
-        }
-
-        return ret;
-    }
-
-    changeSquareItem(i) {
-        if (!this.state.winner) {
-            let localSquareItem = this.state.squareItem.slice();
-            localSquareItem[i] = this.state.isPlayerCurrent ? 'X' : 'O';
-            this.setState({ squareItem: localSquareItem, isPlayerCurrent: !this.state.isPlayerCurrent, winner: this.calcWinner(localSquareItem) });
-        }
-    }
-
     renderSquare(i) {
-        return <Square value={this.state.squareItem[i]} onClick={() => this.changeSquareItem(i)} />;
+        return <Square value={this.props.squareItem[i]} onClick={() => this.props.onChangeItem(i)} />;
     }
 
-    render() {
-        let status = "";
-
-        if (this.state.winner) {
-            status = 'Winner: ' + this.state.winner;
-        } else {
-            status = 'Next player: ' + (this.state.isPlayerCurrent ? 'X' : 'O');
-        }
-
+    render() {        
         return (
-            <div>
-                <div className="status">{status}</div>
+            <div>                
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
--- game/src/components/Game.js
+++ game/src/components/Game.js
@@ -1,20 +1,78 @@
 import React from 'react';
-import Board from './Board'; 
+import Board from './Board';
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {            
+            history: [{ winner: null, currentPlayer: 'X', square: Array(9).fill(null) }]
+        };
+    }
+
+    onGoToStepHandler(step) {
+        this.setState({ history: this.state.history.slice(0, step + 1) });
+    }
+
+    changeSquareItem(i) {        
+        if (!this.state.history[this.state.history.length - 1].winner) {            
+            let currentState = this.state.history[this.state.history.length - 1];
+            let newState = {
+                square: currentState.square.slice(), 
+                winner: null, 
+                currentPlayer: currentState.currentPlayer === 'X' ? 'O' : 'X'
+            };  
+            newState.square[i] = currentState.currentPlayer; 
+            newState.winner = this.calcWinner(newState.square);
+            this.setState({                
+                history: this.state.history.concat([newState])
+            });
+        }
+    }
+
+    calcWinner(squareItem) {
+        let ret = null;
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squareItem[a] &&
+                squareItem[a] === squareItem[b] &&
+                squareItem[a] === squareItem[c]) {
+                ret = squareItem[a];
+                break;
+            }
+        }
+
+        return ret;
+    }
+
+    render() {        
+        let currentState = this.state.history[this.state.history.length - 1]; 
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board squareItem={currentState.square.slice()}
+                        onChangeStatus={(newStatus) => this.onChangeStatusHandler(newStatus)}
+                        onChangeItem={(item) => this.changeSquareItem(item)} />
+                </div>
+                <div className="game-info">
+                    <div>{currentState.winner ? ('Winner: ' + currentState.winner) : ('Player: ' + currentState.currentPlayer)}</div>
+                    <ol>{this.state.history.map((state, index) => { return { text: index ? ('Step ' + index) : 'Start the Game', state: state } })
+                        .map((value, index) => (<li key={index}><button onClick={() => this.onGoToStepHandler(index)}>{value.text}</button>{value.state.square}</li>))}</ol>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Game; 
